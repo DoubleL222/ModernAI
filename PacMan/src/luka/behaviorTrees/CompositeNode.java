1ce58ecb6dafbfe97f357227e3ac73de7d9bbdc0
Luka Locniskar <luka.locniskar@gmail.com>
2018-09-10 00:44:15 +0200

    some work

---
+++ PacMan/src/luka/behaviorTrees/CompositeNode.java
@@ -0,0 +1,13 @@
+package luka.behaviorTrees;
+
+import luka.behaviorTrees.BehaviorTreeNode;
+
+public class CompositeNode extends BehaviorTreeNode{
+	public CompositeNode(NodeType myNodeType) {
+		super(myNodeType);
+		// TODO Auto-generated constructor stub
+	}
+
+	private CompositeType myCompositeType;
+	
+}
---
+++ PacMan/src/luka/behaviorTrees/DecoratorNode.java
@@ -0,0 +1,13 @@
+package luka.behaviorTrees;
+
+import luka.behaviorTrees.BehaviorTreeNode;
+
+public class DecoratorNode extends BehaviorTreeNode {
+	public DecoratorNode(NodeType myNodeType) {
+		super(myNodeType);
+		// TODO Auto-generated constructor stub
+	}
+
+	private DecoratorType myDecoratorType;
+	
+}
