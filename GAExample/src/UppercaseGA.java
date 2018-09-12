3ac57028185f92dc433d986fc32899d3f1aa6d86
Luka Locniskar <luka.locniskar@gmail.com>
2018-09-12 23:57:43 +0200

    EA

--- GAExample/src/UppercaseGA.java
+++ GAExample/src/UppercaseGA.java
@@ -1,7 +1,17 @@
 import java.util.Vector;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+
+import java.lang.reflect.Array;
+import java.util.Collections;
 import java.util.Random;
 
 public class UppercaseGA {
+	private final static float CHANCETOMUTATE = 0.2f;
+	private final static int POPULATION = 100;
+	private final static int GENOTYPELENGTH = 30;
+	private final static int ELITEPERCENTAGE = 10;
+	
 	private class Genotype<T> implements Comparable<Genotype<T>>
 	{
 		public float fitness;
@@ -15,7 +25,7 @@ public class UppercaseGA {
 
 		@Override
 		public int compareTo(Genotype<T> arg0) {
-			return (int) (this.fitness - arg0.fitness);
+			return (int) (arg0.fitness*100 - this.fitness*100);
 		}
 	}
 	
@@ -71,10 +81,128 @@ public class UppercaseGA {
 		}
 	}
 	
+	private Vector<int[]> getElitePopulation(float percentage, Vector<int[]> population) 
+	{
+		Vector<Genotype<int[]>> fullPopulation = new Vector<>();
+		Vector<int[]> elites = new Vector<int[]>();
+		for(int[] gene : population) 
+		{
+			fullPopulation.add(new Genotype<int[]>(GetFitness(gene), gene));
+		}
+		Collections.sort(fullPopulation);
+		
+		/*
+		System.out.println();
+		System.out.println("Sorted:");
+
+		for(Genotype<int[]> g : fullPopulation) 
+		{
+			PrintGenotype(g.genotype);
+			System.out.print("F: "+ g.fitness +"; ");
+		}
+
+		for(int i = 0; i<fullPopulation.size(); i++) 
+		{
+			PrintGenotype(fullPopulation.get(i).genotype);
+			System.out.print("F: "+ fullPopulation.get(i).fitness +"; ");
+		}
+		*/
+		
+		int eliteCount =(int) (percentage/100.0f * population.size());
+		for(int i = 0; i<eliteCount; i++) 
+		{
+			elites.add(fullPopulation.get(i).genotype);
+		}
+		return elites;
+	}
+	
+	private float GetFitness(int[] genotype) 
+	{
+		float fitness = 0;
+		for(int fenotype : genotype) 
+		{
+			fitness+=fenotype;
+		}
+		return fitness/genotype.length;
+	}
+	
+	private float GetMaxFitness(Vector<int[]> population) 
+	{
+		float MaxFitness = Float.MIN_VALUE;
+		for(int[] gene : population) 
+		{
+			float currFitness = GetFitness(gene);
+			
+			if(currFitness>MaxFitness) 
+			{
+				MaxFitness = currFitness;
+			}
+		}
+		return MaxFitness;
+	}
+	
+	private int[] MekeMutant(int[] elite) 
+	{
+		int[] mutant = new int[elite.length];
+		
+		for(int i =0; i< elite.length; i++) 
+		{
+			Random r = new Random();
+			float random = 0f + r.nextFloat() * (1f - 0f);
+			
+			int f = elite[i];
+			
+			if(random < CHANCETOMUTATE) 
+			{
+				if(f == 0) 
+				{
+					f = 1;
+				}
+				else if(f == 1) 
+				{
+					f = 0;
+				}
+			}
+			mutant[i] = f;
+		}
+		return mutant;
+	}
+	
+	private Vector<int[]> MutateElite(Vector<int[]> elites, int populationCount)
+	{
+		Vector<int[]> mutants = new Vector<int[]>();
+		int mutantsPerElite = populationCount/elites.size();
+		for(int i = 0; i < elites.size(); i++) 
+		{
+			for(int j = 0; j < mutantsPerElite; j++) 
+			{
+				mutants.add(MekeMutant(elites.get(i)));
+			}
+		}
+		return mutants;
+	}
+	
 	public static void main(String[] args)
 	{
 		UppercaseGA uGa = new UppercaseGA();
-		Vector<int[]> population = uGa.GeneratePopulation(8, 100);
-		uGa.PrintPopulation(population);
+		Vector<int[]> population = uGa.GeneratePopulation(GENOTYPELENGTH, POPULATION);
+		//uGa.PrintPopulation(population);
+		Vector<int[]> elites = uGa.getElitePopulation(ELITEPERCENTAGE, population);
+		int generation = 0;
+		System.out.println("ELITES");
+		uGa.PrintPopulation(elites);
+		
+		while(uGa.GetMaxFitness(elites)<1) 
+		{
+			System.out.println();
+			System.out.println("-----GENERATION "+generation+"-------");
+			System.out.println();
+			population = uGa.MutateElite(elites, POPULATION);
+			elites = uGa.getElitePopulation(ELITEPERCENTAGE, population);
+			generation++;
+			
+			System.out.println("ELITES");
+			uGa.PrintPopulation(elites);
+		}
 	}
 }
